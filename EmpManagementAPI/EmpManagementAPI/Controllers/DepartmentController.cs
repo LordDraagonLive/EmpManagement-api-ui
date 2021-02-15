a5fdb6c7f0c10f3a91924742c2e3b12a4d9f267e
Buddhi Adhikari <buddhiadikari@live.com>
2021-02-15 18:25:35 +0530

    first commit

---
+++ EmpManagementAPI/EmpManagementAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using EmpManagementAPI.Models;
+
+namespace EmpManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {   
+            // using sql query only for testing use entity framework
+            string query = @"select DepartmentId, DepartmentName from dbo.Department";
+            DataTable dataTable = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader sqlDataReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand sqlCommand = new SqlCommand(query, myCon))
+                {
+                    sqlDataReader = sqlCommand.ExecuteReader();
+                    dataTable.Load(sqlDataReader);
+
+                    sqlDataReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(dataTable);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Department department)
+        {
+            // using sql query only for testing use entity framework
+            string query = @"insert into dbo.Department values
+                            ('"+ department.DepartmentName +@"')";
+            DataTable dataTable = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader sqlDataReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand sqlCommand = new SqlCommand(query, myCon))
+                {
+                    sqlDataReader = sqlCommand.ExecuteReader();
+                    dataTable.Load(sqlDataReader);
+
+                    sqlDataReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully!");
+        }
+
+
+        [HttpPut]
+        public JsonResult Put(Department department)
+        {
+            // using sql query only for testing use entity framework
+            string query = @"update dbo.Department set
+                            DepartmentName = '" + department.DepartmentName + @"'
+                            where DepartmentId = " + department.DepartmentId + @" ";
+            DataTable dataTable = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader sqlDataReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand sqlCommand = new SqlCommand(query, myCon))
+                {
+                    sqlDataReader = sqlCommand.ExecuteReader();
+                    dataTable.Load(sqlDataReader);
+
+                    sqlDataReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully!");
+        }
+
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            // using sql query only for testing use entity framework
+            string query = @"delete from dbo.Department
+                            where DepartmentId = " + id + @" ";
+            DataTable dataTable = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader sqlDataReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand sqlCommand = new SqlCommand(query, myCon))
+                {
+                    sqlDataReader = sqlCommand.ExecuteReader();
+                    dataTable.Load(sqlDataReader);
+
+                    sqlDataReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully!");
+        }
+    }
+}
---
+++ EmpManagementAPI/EmpManagementAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,199 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using EmpManagementAPI.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace EmpManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public EmployeeController(IConfiguration configuration, IWebHostEnvironment webHostEnvironment)
+        {
+            _configuration = configuration;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            // using sql query only for testing use entity framework
+            string query = @"select EmployeeId, EmployeeName, Department,
+                            convert(varchar(10), DateOfJoining,120) as DateOfJoining,
+                            PhotoFileName from dbo.Employee";
+            DataTable dataTable = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader sqlDataReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand sqlCommand = new SqlCommand(query, myCon))
+                {
+                    sqlDataReader = sqlCommand.ExecuteReader();
+                    dataTable.Load(sqlDataReader);
+
+                    sqlDataReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(dataTable);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Employee employee)
+        {
+            // using sql query only for testing use entity framework
+            string query = @"insert into dbo.Employee
+                            (EmployeeName, Department, DateOfJoining, PhotoFileName)
+                            values(
+                            '" + employee.EmployeeName + @"'
+                            ,'" + employee.Department + @"'
+                            ,'" + employee.DateOfJoining + @"'
+                            ,'" + employee.PhotoFileName + @"'
+                            )";
+            DataTable dataTable = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader sqlDataReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand sqlCommand = new SqlCommand(query, myCon))
+                {
+                    sqlDataReader = sqlCommand.ExecuteReader();
+                    dataTable.Load(sqlDataReader);
+
+                    sqlDataReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully!");
+        }
+
+
+        [HttpPut]
+        public JsonResult Put(Employee employee)
+        {
+            // using sql query only for testing use entity framework
+            string query = @"update dbo.Employee set
+                            EmployeeName = '" + employee.EmployeeName + @"'
+                            ,Department = '" + employee.Department + @"'
+                            ,DateOfJoining = '" + employee.DateOfJoining + @"'
+                            where EmployeeId = " + employee.EmployeeId + @" ";
+            DataTable dataTable = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader sqlDataReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand sqlCommand = new SqlCommand(query, myCon))
+                {
+                    sqlDataReader = sqlCommand.ExecuteReader();
+                    dataTable.Load(sqlDataReader);
+
+                    sqlDataReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully!");
+        }
+
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            // using sql query only for testing use entity framework
+            string query = @"delete from dbo.Employee
+                            where EmployeeId = " + id + @" ";
+            DataTable dataTable = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader sqlDataReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand sqlCommand = new SqlCommand(query, myCon))
+                {
+                    sqlDataReader = sqlCommand.ExecuteReader();
+                    dataTable.Load(sqlDataReader);
+
+                    sqlDataReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully!");
+        }
+
+        [Route("SaveFile")]
+        [HttpPost]
+        public JsonResult SaveFile()
+        {
+            try
+            {
+                var httpRequest = Request.Form;
+                var postedFile = httpRequest.Files[0];
+                string filename = postedFile.FileName;
+                var physicalPath = _webHostEnvironment.ContentRootPath + "/Photos/" + filename;
+
+                using (var stream = new FileStream(physicalPath, FileMode.Create))
+                {
+                    postedFile.CopyTo(stream);
+                }
+
+                return new JsonResult(filename);
+
+            }
+            catch (Exception)
+            {
+
+                return new JsonResult("some.png");
+            }
+        }
+
+        [Route("GetAllDepartmentNames")]
+        [HttpGet]
+        public JsonResult GetAllDepartmentNames()
+        {
+
+            // using sql query only for testing use entity framework
+            string query = @"select DepartmentName from dbo.Department";
+            DataTable dataTable = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader sqlDataReader;
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand sqlCommand = new SqlCommand(query, myCon))
+                {
+                    sqlDataReader = sqlCommand.ExecuteReader();
+                    dataTable.Load(sqlDataReader);
+
+                    sqlDataReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(dataTable);
+        }
+    }
+}
